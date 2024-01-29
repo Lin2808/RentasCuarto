949968599b5e6349d6178d3228a8f4e3af1dcaee
linja <linjaviers@hotmail.com>
2024-01-28 19:43:33 -0500

    Primer commit

---
+++ src/main/java/com/example/RentasCuarto/RentasCuartoApplication.java
@@ -0,0 +1,13 @@
+package com.example.RentasCuarto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentasCuartoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentasCuartoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RentasCuarto/RentasCuartoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RentasCuarto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentasCuartoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
